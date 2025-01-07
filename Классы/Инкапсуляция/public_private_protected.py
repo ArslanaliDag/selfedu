3084cd9df145c0adcdce7634ef84a3b64b83ff65
Arslanali Suleymanov <a.suleymanov@lekar05.ru>
2025-01-07 15:03:55 +0300

    install accessify

---
+++ Классы/Инкапсуляция/public_private_protected.py
@@ -0,0 +1,34 @@
+class Point:
+    def __init__(self, x = 0, y = 0):
+        # у переменной x тип private, а если с одни подчеркиванием то тип protected
+        self.__x = self.__y = 0
+        if self.__check_value(x) and self.__check_value(y):
+            self.__x = x
+            self.__y = y
+        else:
+            raise ValueError("Координаты должны быть числами!")
+    
+    
+    @classmethod
+    # инкапсулированными могут быть и методы
+    def __check_value(cls, arg):
+        return type(arg) in(int, float) 
+    
+    
+    def set_coord(self, x, y):
+        # if type(x) in(int, float) and type(y) in(int, float):
+        # ВОт для таких целей, к примеру, можно и использвоать методы класса.
+        if self.__check_value(x) and self.__check_value(y):
+            self.__x = x
+            self.__y = y
+        else:
+            raise ValueError("Координаты должны быть числами!")
+    
+    
+    def get_coord(self):
+        return  self.__x, self.__y
+    
+    
+pt = Point()
+pt.set_coord(2, 33)
+print(pt.get_coord())
